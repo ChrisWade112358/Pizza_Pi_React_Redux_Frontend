5027a94a2a2774583064341fa2c8df809de754cd
swade781977 <swade781977@yahoo.com>
2021-08-08 23:35:57 -0400

    added user actions

--- src/Actions/users.js
+++ src/Actions/users.js
@@ -2,5 +2,59 @@ import axios from axios;
 
 const address = 'to be determined'
 export const addUser = user => {
-    
+    return (dispatch) => {
+        fetch(`${address}users`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                user,
+                withCredentials: true,
+            })
+        })
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            user = Object.assign({}, data.user, {jwt: data.jwt});
+            localStorage.setItem('jwtToken', data.jwt);
+            localStorage.setItem('user_id', data.user.id)
+            dispatch({type: 'ADD_USER', payload: user});
+            
+
+        })
+        .catch(error => {
+            console.log("registration error", error);
+        })
+    }
+}
+
+export const userLogin = user => {
+    return (dispatch) => {
+        fetch(`${address}auth`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                user,
+                withCredentials: true,
+            })
+        })
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            console.log(data)
+            user = Object.assign({}, data.user, {jwt: data.jwt}, {isLoggedIn: true});
+            localStorage.setItem('jwtToken', data.jwt);
+            dispatch({type: 'ADD_USER', payload: user});
+        })
+        .catch(error => {
+            console.log("registration error", error);
+        })
+    }
 }
